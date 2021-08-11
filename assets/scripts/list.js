f85f4d4c0e7a3a6ead5e5340de93940e45f33f39
LaurePiechaczyk <laure.piecha@gmail.com>
2021-08-11 09:37:12 +0000

    increased veb list

--- assets/scripts/list.js
+++ assets/scripts/list.js
@@ -51,10 +51,6 @@ export let verbsWithPrepositions = [{
         preposition: "von",
     },
     {
-        verb: "bestehen",
-        preposition: "aus",
-    },
-    {
         verb: "riechen",
         preposition: "nach",
     },
@@ -93,5 +89,157 @@ export let verbsWithPrepositions = [{
     {
         verb: "bitten",
         preposition: "um",
+    },
+    {
+        verb: "abstimmen",
+        preposition: "über",
+    },
+    {
+        verb: "aufhören",
+        preposition: "mit",
+    },
+    {
+        verb: "aufpassen",
+        preposition: "auf",
+    },
+    {
+        verb: "sich aufregen",
+        preposition: "über",
+    },
+    {
+        verb: "ausgeben",
+        preposition: "für",
+    },
+    {
+        verb: "berichten",
+        preposition: "über",
+    },
+    {
+        verb: "sich beschäftigen",
+        preposition: "mit",
+    },
+    {
+        verb: "sich bewerben",
+        preposition: "um",
+    },
+    {
+        verb: "sich beziehen",
+        preposition: "auf",
+    },
+    {
+        verb: "diskutieren",
+        preposition: "über",
+    },
+    {
+        verb: "einladen",
+        preposition: "zu",
+    },
+    {
+        verb: "sich entscheiden",
+        preposition: "für",
+    },
+    {
+        verb: "sich erholen",
+        preposition: "von",
+    },
+    {
+        verb: "sich erkundigen",
+        preposition: "nach",
+    },
+    {
+        verb: "ersetzen",
+        preposition: "durch",
+    },
+    {
+        verb: "fragen",
+        preposition: "nach",
+    },
+    {
+        verb: "folgen",
+        preposition: "auf",
+    },
+    {
+        verb: "gehen",
+        preposition: "um",
+    },
+    {
+        verb: "gehören",
+        preposition: "zu",
+    },
+    {
+        verb: "glauben",
+        preposition: "an",
+    },
+    {
+        verb: "gratulieren",
+        preposition: "zu",
+    },
+    {
+        verb: "sich handeln",
+        preposition: "um",
+    },
+    {
+        verb: "hören",
+        preposition: "auf",
+    },
+    {
+        verb: "hoffen",
+        preposition: "auf",
+    },
+    {
+        verb: "(sich) informieren",
+        preposition: "über",
+    },
+    {
+        verb: "sich konzentrieren",
+        preposition: "auf",
+    },
+    {
+        verb: "protestieren",
+        preposition: "gegen",
+    },
+    {
+        verb: "reagieren",
+        preposition: "auf",
+    },
+    {
+        verb: "sich schämen",
+        preposition: "für",
+    },
+    {
+        verb: "schimpfen",
+        preposition: "über",
+    },
+    {
+        verb: "stehen",
+        preposition: "auf",
+    },
+    {
+        verb: "überzeugen",
+        preposition: "von",
+    },
+    {
+        verb: "unterrichten",
+        preposition: "über",
+    },
+    {
+        verb: "sich verabschieden",
+        preposition: "von",
+    },
+    {
+        verb: "sich verlieben",
+        preposition: "in",
+    },
+    {
+        verb: "sich vorbereiten",
+        preposition: "auf",
+    },
+    {
+        verb: "sich wundern",
+        preposition: "über",
+    },
+    {
+        verb: "zweifeln",
+        preposition: "an",
     },
 ];
